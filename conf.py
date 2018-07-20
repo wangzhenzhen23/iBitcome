a6505ff2ca865fcaa67c388decbe96d918dd5ea4
yangyiyong <yiyong.yang@bitmain.com>
2018-07-20 09:40:36 +0800

    修改配置

--- conf.py
+++ conf.py
@@ -16,17 +16,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import recommonmark
-from recommonmark.transform import AutoStructify
-def setup(app):
-    app.add_config_value('recommonmark_config', {
-            'url_resolver': lambda url: github_doc_root + url,
-            'auto_toc_tree_section': 'Contents',
-            }, True)
-    app.add_transform(AutoStructify)
-
-
-# -- Project information -----------------------------------------------------
 
 project = u'iBitcome'
 copyright = u'2018, wangzheng'
